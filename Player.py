3797c8deda8ff9a1f54aa49a3dc037988db4a69a
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-10-16 11:14:01 +0300

    now it creates json files for every player

--- Player.py
+++ Player.py
@@ -1,3 +1,4 @@
+import json
 import os
 from tools import generateID, assign_random_name
 
@@ -7,14 +8,20 @@ class Player:
         self.id = generateID()
         self.write_player_file()
 
+    def to_dict(self):
+        return {
+            "player_id": self.id,
+            "player_name": self.name
+        }
+
     # This is for creating a file with user information
     def write_player_file(self):
         folder_path = "data/players"
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
-        file_path = os.path.join(folder_path, f"{self.get_id()}.txt")
+        file_path = os.path.join(folder_path, f"{self.get_id()}.json")
         with open(file_path, "w") as file:  # Changed from folder_path to file_path
-            file.write(self.__str__())
+            json.dump(self.to_dict(), file)
         print(f"Wrote player file for {self.get_id()} to {file_path}\n")
 
     def __str__(self):
