12ccbb6a54c7d84fe74544130942267d4eac0afc
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-10-29 15:06:30 +0200

    fixed more

--- Match.py
+++ Match.py
@@ -9,4 +9,20 @@ class Match(EntityBase):
             self.team_2 = load_random_team()
             if not have_common_players(self.team_1, self.team_2):
                 break
+        self.set_winner()
+        self.set_loser()
         super().__init__(entity_type = "match")
+    def set_winner(self):
+        if self.team_1 == self.team_2:
+            raise ValueError("both teams cannot win or lose!\n")
+        elif self.team_1["team_status"] == "win":
+            self.winner = self.team_1["team_name"]
+        elif self.team_2["team_status"] == "win":
+            self.winner = self.team_2["team_name"]
+    def set_loser(self):
+        if self.team_1 == self.team_2:
+            raise ValueError("both teams cannot win or lose!\n")
+        elif self.team_1["team_status"] == "loss":
+            self.loser = self.team_1["team_name"]
+        elif self.team_2["team_status"] == "loss":
+            self.loser = self.team_2["team_name"]
